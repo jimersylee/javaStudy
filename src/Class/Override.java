63fc5ea1ae51410d72ca95c73d89f767db640c05
Jimersy Lee <jimersylee@gmail.com>
2017-03-11 17:25:55 +0800

    this

--- src/Class/Override.java
+++ src/Class/Override.java
@@ -49,12 +49,46 @@ public class Override {
 
 class Hero {
     String name;
+    int hp;
+
+    /**
+     * 给类中name赋值
+     * 方法名和属性名一样,只能访问到参数name;
+     * @param name:姓名
+     */
+    public void setName1(String name){
+        name=name;
+    }
+
+    /**
+     * 为了避免setName1中的问题,参数名不得不适用其他变量名
+     */
+    public void setName2(String heroName){
+        name=heroName;
+    }
+
+    /**
+     * 通过this访问属性,就可以使参数名与类成员名同名
+     */
+    public void setName3(String name){
+        this.name=name;
+    }
+
+    /*public Hero(String name){
+        this.name=name;
+    }*/
+    /*public Hero(String name,int hp){
+        this(name);
+        this.hp=hp;
+    }*/
 }
 
 /**
  * 方法重载,方法名是一样的,但是参数类型不一样,会根据传递的参数类型以及数量,自动调用对应的方法
  */
 class ADHero extends Hero {
+
+
     public void attack(Hero h1) {
         System.out.println(name + "对" + h1.name + "进行了一次攻击");
     }
